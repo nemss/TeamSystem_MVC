fed78837d8907992ebfcac390e1c97c457118090
Симеон Мечков <montishak@gmail.com>
2018-02-22 00:36:21 +0200

    Fixed JQuery on Admin Panel

--- TeamSystem/TeamSystem.Web/wwwroot/js/site.js
+++ TeamSystem/TeamSystem.Web/wwwroot/js/site.js
@@ -1,21 +1,21 @@
 ﻿// Write your JavaScript code.
 $(document).ready(function () {
-    $("#dropdownMenuAdminPanel").hover(
-        function () {
-            $(".dropdown-menu").stop(true, true).slideDown("fast")
-        },
-        function () {
-            if ($(".dropdown-menu").is(":hover")) { }
-            else {
-                $(".dropdown-menu").stop(true, true).slideUp("fast")
-            }
-        }
-    )
-});
-$(document).ready(function () {
-    $(".dropdown-menu").mouseleave(
-        function () {
-            $(".dropdown-menu").stop(true, true).slideUp("fast")
-        }
-    )
+    if ($(window).width() > 768) {
+            $("#dropdownMenuAdminPanel").hover(
+                function () {
+                    $(".dropdown-menu").stop(true, true).slideDown("fast")
+                },
+                function () {
+                    if ($(".dropdown-menu").is(":hover")) { }
+                    else {
+                        $(".dropdown-menu").stop(true, true).slideUp("fast")
+                    }
+                }
+            ),
+            $(".dropdown-menu").mouseleave(
+                function () {
+                    $(".dropdown-menu").stop(true, true).slideUp("fast")
+                }
+            )
+    }
 });
